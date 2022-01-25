a0e51bfb6f598c3678f47333a3af5662aa78de77
alyssadurante <alyssa.durante@answerrocket.com>
2022-01-25 00:12:05 -0500

    intial design changes

--- content/introduction.md
+++ content/introduction.md
@@ -4,7 +4,7 @@ metaTitle: "This is the title tag of this page"
 metaDescription: "This is the meta description"
 ---
 
-Some introduction text. Lists out all the headings from h1 to h6. Markdown link handling for relative and absolute URLs. Easy to customise.
+kjnkjn
 
 # Heading H1
 Heading 1 text
--- src/components/sidebar/index.js
+++ src/components/sidebar/index.js
@@ -18,7 +18,7 @@ const ListItem = styled(({ className, active, level, ...props }) => {
   list-style: none;
 
   a {
-    color: #5c6975;
+    color: #374151;
     text-decoration: none;
     font-weight: ${({ level }) => (level === 0 ? 700 : 400)};
     padding: 0.45rem 0 0.45rem ${(props) => 2 + (props.level || 0) * 1}rem;
@@ -26,7 +26,7 @@ const ListItem = styled(({ className, active, level, ...props }) => {
     position: relative;
 
     &:hover {
-      color: #1ed3c6 !important;
+      color: #111827 !important;
     }
 
     ${(props) =>
